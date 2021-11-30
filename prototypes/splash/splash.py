de46d591fd28996b7fc6da9db17256c77e449d47
mcclellangg <81567927+mcclellangg@users.noreply.github.com>
2021-11-30 12:06:16 -0500

    initialize splash app

---
+++ prototypes/splash/splash.py
@@ -0,0 +1,20 @@
+# splash.py
+"""This will be a prototype for my splash screen that first comes up when the user opens the application.
+Features that it will have include:
+- Open Existing Company
+- Initialize New Company
+- About
+"""
+import tkinter as tk
+
+
+# Tkinter GUI
+root = tk.Tk()
+root.title("Simple Pay Splash")
+root.geometry("650x800")
+
+# Create Labels for options:
+
+
+# Start the event loop
+root.mainloop()
