a3abd03ab26bedccb837eb110624fc9536006904
mcclellangg <81567927+mcclellangg@users.noreply.github.com>
2021-12-01 15:36:52 -0500

    created a framework layout for splash page

--- prototypes/splash/splash.py
+++ prototypes/splash/splash.py
@@ -8,12 +8,38 @@ Features that it will have include:
 import tkinter as tk
 
 
-# Tkinter GUI
+# Tkinter GUI:
 root = tk.Tk()
 root.title("Simple Pay Splash")
-root.geometry("650x800")
+root.geometry("600x800")
 
-# Create Labels for options:
+# Logo :
+logo = tk.Label(root, text="Simple Pay", borderwidth=2, relief="raised")
+logo.config(font=("Helvatical bold", 20))
+logo.grid(row=0, column=0, ipadx=50, ipady=50, padx=150, pady=20, columnspan=3)
+
+# Set parameters for dropdown:
+company_files = [
+    "path/to/fooCompany",
+    "path/to/fizzCompany",
+    "path/to/awesomeCompany",
+]
+
+selected = tk.StringVar()
+selected.set("Choose Company")
+
+# Menu Labels:
+select_file = tk.Label(root, text="Select Company file: ")
+select_file.grid(row=1, column=0)
+dropdown = tk.OptionMenu(root, selected, *company_files)
+dropdown.config(width=35)
+dropdown.grid(row=1, column=1)
+open_file = tk.Button(root, text='Open')
+open_file.grid(row=1, column=2)
+create_file = tk.Label(root, text="Create a new Company file")
+create_file.grid(row=2, column=0, padx=5, pady=5)
+about = tk.Label(root, text="About")
+about.grid(row=3, column=0, padx=5, pady=5)
 
 
 # Start the event loop
