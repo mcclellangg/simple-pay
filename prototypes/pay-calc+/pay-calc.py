e4825cb70f5cb148a21bf80bea78c559e7881278
mcclellangg <81567927+mcclellangg@users.noreply.github.com>
2021-12-15 14:57:14 -0500

    added tree display for calculated paychecks in pay-calc+

--- prototypes/pay-calc+/pay-calc.py
+++ prototypes/pay-calc+/pay-calc.py
@@ -16,50 +16,64 @@
     }
 """
 import tkinter as tk
+from tkinter import ttk
 import calculations as calc
 
 root = tk.Tk()
 root.title("Pay-Calc+")
-root.geometry("600x600")
+root.geometry("750x450")
 
+count = 0  # iid for paychecks in treeview UNIQUE
 
 # Command functions
 def create_paycheck():
     """Takes the user inputs, passes them to the calculate function, and generates a paycheck object that is
     ready to display or add to the database."""
+    global count
     # Get info from entry fields, and clean it for calc function
     user_input = {
-        'name': nameField.get(),
-        'exemptions': int(exemptionsField.get()),
-        'gross pay' : round(float(grossField.get()), 2)
+        "name": nameField.get(),
+        "exemptions": int(exemptionsField.get()),
+        "gross pay": round(float(grossField.get()), 2),
     }
     paycheck = calc.calculate_deductions(user_input)
-    # Display the info:
-    for widget in displayFrame.winfo_children():
-        widget.destroy()
-    infoLbl = tk.Label(displayFrame, text=paycheck, width=60, wraplength=500)
-    infoLbl.pack()
-
+    # Insert the paycheck directly into the tree view:
+    tree_display.insert(
+        parent="",
+        index="end",
+        iid=count,
+        text="12/15/21",
+        values=(
+            paycheck["name"],
+            paycheck["exemptions"],
+            paycheck["gross pay"],
+            paycheck["federal"],
+            paycheck["social security"],
+            paycheck["medicare"],
+            paycheck["state"],
+            paycheck["net"],
+            paycheck["net pay"],
+        ),
+    )
+    count += 1
 
 
 def update_records():
-    """Takes the paystub that has been created, attaches a timestamp, and puts it into the database."""
+    """Read the paycheck objects from the list and add them to the record base."""
+    # TODO Will complete this functionality once db framework is established in next phase.
     pass
-
 
 
 nameLbl = tk.Label(root, text="Enter employee name : ", anchor="w")
 nameField = tk.Entry(root)
-exemptionsLbl = tk.Label(root, text="Enter exemptions :  ", anchor="w")  # Updated from dependents
+exemptionsLbl = tk.Label(
+    root, text="Enter exemptions :  ", anchor="w"
+)  # Updated from dependents
 exemptionsField = tk.Entry(root)
 grossLbl = tk.Label(root, text="Enter gross pay: ", anchor="w")
 grossField = tk.Entry(root)
 calcBtn = tk.Button(root, text="Calculate", command=create_paycheck)
 updateBtn = tk.Button(root, text="Add Entry", command=update_records)
-
-displayFrame = tk.LabelFrame(root, text="Display Info", width=500, height=400, padx=125, pady=150)
-msgLbl = tk.Label(displayFrame, text="Troubleshoot Here", width=40)
-
 
 nameLbl.config(width=18)
 nameField.config(width=40)
@@ -79,8 +93,50 @@ grossField.grid(row=2, column=1, pady=(0, 12))
 calcBtn.grid(row=3, column=0, padx=(60, 8), pady=(0, 8))
 updateBtn.grid(row=3, column=1, pady=(0, 8))
 
-displayFrame.grid(row=4, column=0, columnspan=2, padx=(40, 30), pady=(20, 0))
-displayFrame.pack_propagate(False)
-msgLbl.pack()
+# Add the tree:
+displayFrame = tk.LabelFrame(root, text="Paychecks")
+displayFrame.grid(row=4, column=0, columnspan=2, padx=(30, 0))
+tree_display = ttk.Treeview(displayFrame)
+
+# Define the Columns:
+tree_display["columns"] = (
+    "Name",
+    "Exemptions",
+    "Gross Pay",
+    "Federal",
+    "Social",
+    "Medicare",
+    "State",
+    "Net",
+    "Net Pay",
+)
+# Format the columns:
+tree_display.column("#0", width=80, minwidth=25)  # '#0' is the phantom column
+tree_display.column("Name", anchor="w", width=60)
+tree_display.column("Exemptions", anchor="center", width=80)
+tree_display.column("Gross Pay", anchor="w", width=60)
+tree_display.column("Federal", anchor="center", width=60)
+tree_display.column("Social", anchor="center", width=60)
+tree_display.column("Medicare", anchor="center", width=60)
+tree_display.column("State", anchor="center", width=60)
+tree_display.column("Net", anchor="center", width=60)
+tree_display.column("Net Pay", anchor="center", width=60)
+
+# Create Headings:
+tree_display.heading("#0", text="Timestamp", anchor="w")
+tree_display.heading("Name", text="Name", anchor="w")
+tree_display.heading("Exemptions", text="Exemptions", anchor="w")
+tree_display.heading("Gross Pay", text="Gross Pay", anchor="w")
+tree_display.heading("Federal", text="Federal", anchor="center")
+tree_display.heading("Social", text="Social", anchor="center")
+tree_display.heading("Medicare", text="Medicare", anchor="center")
+tree_display.heading("State", text="State", anchor="center")
+tree_display.heading("Net", text="Net", anchor="center")
+tree_display.heading("Net Pay", text="Net Pay", anchor="center")
+
+# Add Sample Data:
+# tree_display.insert(parent='', index='end', iid='s', text="12/15/21", values=('Jim', 2, "$1,200", 23, 12, 18, 43, 186, "987.50"))
+
+tree_display.pack()
 
 root.mainloop()
